3a64404063f1f2d0ce0f4af7b5d680f1c079f2d9
MJ Rodríguez <recogemoras@gmail.com>
2022-12-10 23:25:32 +0000

    latest optimization

--- .build/index.js
+++ .build/index.js
@@ -1,9 +1,9 @@
 const gifts = ["cat", "game", "socks"];
 const wrapped = wrapping(gifts);
 function wrapping(gifts2) {
-  let wrapped2 = [];
+  let wrapped2 = [], regexp = /./g;
   for (let i in gifts2) {
-    let asteriscos = gifts2[i].replace(new RegExp(/./g), "*") + "**";
+    let asteriscos = gifts2[i].replace(regexp, "*") + "**";
     wrapped2.push(asteriscos + "\n*" + gifts2[i] + "*\n" + asteriscos);
   }
   return wrapped2;
--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ const gifts = ['cat', 'game', 'socks']
 const wrapped = wrapping(gifts)
 
 function wrapping(gifts) {
-    let wrapped: string [] = []
+    let wrapped: string [] = [], regexp: RegExp = /./g
     for (let i in gifts) {
-        let asteriscos = gifts[i].replace(new RegExp(/./g), "*") + "**";
+        let asteriscos = gifts[i].replace(regexp, "*") + "**";
         wrapped.push(asteriscos + "\n*" + gifts[i] + "*\n" + asteriscos)
     }
     return wrapped
